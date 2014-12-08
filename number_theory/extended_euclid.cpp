0d89a358af0eff1600086345d1b8a8c077ad44e9
fnasimbd <f.nasim01@gmail.com>
2014-12-08 21:33:15 +0600

    made global variables x2 and y2 reference parameters

--- number_theory/extended_euclid.cpp
+++ number_theory/extended_euclid.cpp
@@ -3,13 +3,11 @@
 
 using namespace std;
 
-int x1, y1;
-int x2, y2;
-
-int extended_euclid(int a, int b)
+int extended_euclid(int a, int b, int& x2, int& y2)
 {
     int q; 
     int r = a % b;
+    int x1, y1;
 
     x1 = 1, y1 = 0;
     x2 = 0, y2 = 1;
@@ -34,12 +32,13 @@ int extended_euclid(int a, int b)
 int main()
 {
     int a, b;
+    int x, y;
 
     scanf("%d %d", &a, &b);
 
-    int g = extended_euclid(a, b);
+    int g = extended_euclid(a, b, x, y);
 
-    printf("%d %d %d\n", g, x2, y2);
+    printf("%d %d %d\n", g, x, y);
 
     return 0;
 }
